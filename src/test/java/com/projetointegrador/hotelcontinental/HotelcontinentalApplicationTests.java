c4da0a485e0afd76f70b051850b2cd4f9a7c069f
Adrian Herrmann <lumbia.85@gmail.com>
2025-03-20 00:09:47 -0300

    Projeto inicial

---
+++ src/test/java/com/projetointegrador/hotelcontinental/HotelcontinentalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetointegrador.hotelcontinental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelcontinentalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
