e677fa51425fc0a4a0eccb4c92262b1b3f206a56
Adrian Herrmann <lumbia.85@gmail.com>
2025-04-28 17:33:07 -0300

    Criação do projeto

---
+++ src/main/java/com/api/projetohotelaria/ProjetohotelariaApplication.java
@@ -0,0 +1,13 @@
+package com.api.projetohotelaria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetohotelariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetohotelariaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/projetohotelaria/ProjetohotelariaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.projetohotelaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetohotelariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
